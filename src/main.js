9f47b2ddedc9e3bceef26c1b40b71cc0ada16738
bart c <kaszikasz@wp.pl>
2022-06-05 20:10:48 +0100

    slideshow with responsive design - working

--- src/main.js
+++ src/main.js
@@ -1,41 +1,79 @@
 
-// for best performance:
-// ---------------------
-//
-// 1. set all img to one size
-// 2. set 
 
+// ......................................................
+
+// a) VARIABLES
+//----------------
+
+// slideshow elements (img)
 const imgShow1 = document.getElementById("show-img-1")
 const imgShow2 = document.getElementById("show-img-2")
 const imgShow3 = document.getElementById("show-img-3")
 
+// slideshow animations
 const aIN = "animation__in";
 const aOUT = "animation__out";
 
-function slideShow (time) {
+// for slideshow responsive width
+let slideshow = document.getElementById("slideshow");
+
+
+
+// b) EL
+//----------
+
+window.addEventListener("resize", responsiveWidth);
+
+
+  
+// c) FUNCTIONS
+//---------------
+
+// slideshow. "time" - distance in ms between animation "in" and "out" (should be the same as "--animation-out-delay" in CSS )
+function slideShow (img1, img2, img3, time) {
 
     setTimeout(()=>{
 
-        setTimeout(()=>imgShow1.classList.add(aIN),0)
-        setTimeout(()=>imgShow2.classList.remove(aIN),0.06*time)
-        setTimeout(()=>imgShow2.classList.add(aIN),time)
-        setTimeout(()=>imgShow3.classList.remove(aIN),time+(0.06*time))
-        setTimeout(()=>imgShow3.classList.add(aIN),time*2)        
-        setTimeout(()=>imgShow1.classList.remove(aIN),2*time+(0.06*time))
+        setTimeout(()=>img1.classList.add(aIN),0)
+        setTimeout(()=>img2.classList.remove(aIN),0.06*time)
+        setTimeout(()=>img2.classList.add(aIN),time)
+        setTimeout(()=>img3.classList.remove(aIN),time+(0.06*time))
+        setTimeout(()=>img3.classList.add(aIN),time*2)        
+        setTimeout(()=>img1.classList.remove(aIN),2*time+(0.06*time))
     
     }, 0)
     
     setInterval(()=>{
 
-        setTimeout(()=>imgShow1.classList.add(aIN),0)
-        setTimeout(()=>imgShow2.classList.remove(aIN),0.06*time)
-        setTimeout(()=>imgShow2.classList.add(aIN),time)
-        setTimeout(()=>imgShow3.classList.remove(aIN),time+(0.06*time))
-        setTimeout(()=>imgShow3.classList.add(aIN),time*2)        
-        setTimeout(()=>imgShow1.classList.remove(aIN),2*time+(0.06*time))
+        setTimeout(()=>img1.classList.add(aIN),0)
+        setTimeout(()=>img2.classList.remove(aIN),0.06*time)
+        setTimeout(()=>img2.classList.add(aIN),time)
+        setTimeout(()=>img3.classList.remove(aIN),time+(0.06*time))
+        setTimeout(()=>img3.classList.add(aIN),time*2)        
+        setTimeout(()=>img1.classList.remove(aIN),2*time+(0.06*time))
     
     }, time*3)
 }
 
-slideShow(5000)
+// --> sets the "height" for slideshow to be responsive 
+// --> sets CSS variables when "slideshow" changes width, for animation to work properly
+function responsiveWidth() {
+
+    const slideshowWidth = slideshow.offsetWidth;
+
+    document.documentElement.style
+    .setProperty('--img-distance-X', `${slideshowWidth}px`);
+    document.documentElement.style
+    .setProperty('--img-distance-X-minus', `-${slideshowWidth}px`);
+
+    slideshow.style.height = `${slideshowWidth/4}px`
+
+  }
+
+//..................................................
+
+
+// d) DECLARATION
+
+slideShow(imgShow1, imgShow2, imgShow3, 5000)
 
