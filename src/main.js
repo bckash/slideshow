a41916052c4c37835175bffb5249e68989cce1c0
bart c <kaszikasz@wp.pl>
2022-06-06 19:32:59 +0100

    slideshow + pagination. responsive + js abstracted

--- src/main.js
+++ src/main.js
@@ -1,62 +1,99 @@
 
 
-// ......................................................
 
-// a) VARIABLES
+// ---> VARIABLES
 //----------------
 
-// slideshow elements (img)
-const imgShow1 = document.getElementById("show-img-1")
-const imgShow2 = document.getElementById("show-img-2")
-const imgShow3 = document.getElementById("show-img-3")
+// slideshow container
+const slideshow = document.getElementById("slideshow");
 
 // slideshow animations
-const aIN = "animation__in";
-const aOUT = "animation__out";
+let animations = {
+    imgIn : "animation__in",
+    imgOut : "animation__out",
+    pag : "animation__pagination"
+}
 
-// for slideshow responsive width
-let slideshow = document.getElementById("slideshow");
+// building "imgParArray" :  array of objects containing "img elements" and "pagination elements" as properties - for slideshowAbstract()
+const pagIcons = document.querySelectorAll(".pag-icons")
+const showImages = document.querySelectorAll(".show-images");
+
+let imgParArray = [];
+
+function toArray (pags, imgs, arr) {
+
+        pags.forEach(el => {
+            let obj = {};
+            obj.pag = el;
+            arr.push(obj)
+        })
+
+        for (i=0; i<arr.length; i++) {
+            arr[i].img = imgs[i]
+        }
+
+
+}
+
+toArray (pagIcons, showImages, imgParArray)
 
 
 
-// b) EL
+// ---> EL 
 //----------
 
 window.addEventListener("resize", responsiveWidth);
 
 
-  
-// c) FUNCTIONS
-//---------------
 
-// slideshow. "time" - distance in ms between animation "in" and "out" (should be the same as "--animation-out-delay" in CSS )
-function slideShow (img1, img2, img3, time) {
+// --> FUNCTIONS
+//----------------
 
-    setTimeout(()=>{
 
-        setTimeout(()=>img1.classList.add(aIN),0)
-        setTimeout(()=>img2.classList.remove(aIN),0.06*time)
-        setTimeout(()=>img2.classList.add(aIN),time)
-        setTimeout(()=>img3.classList.remove(aIN),time+(0.06*time))
-        setTimeout(()=>img3.classList.add(aIN),time*2)        
-        setTimeout(()=>img1.classList.remove(aIN),2*time+(0.06*time))
+// slideshow 
+function slideShowAbstract (pagsImgs, animationsObj, time) {
+
+    // get values from "imgParArray"
+    function makeVar (arr, index, prop) {
+        return Object.values(arr[index])[prop]
+    }
+
+    const aIN = Object.values(animationsObj)[0];
+    const aPAG = Object.values(animationsObj)[2];
+    const t = time/3;
+
+        function backend () {
+            for (let i=0; i<pagsImgs.length; i++) {
+
+                let img = makeVar(pagsImgs, i, 1);
+                let pag = makeVar(pagsImgs, i, 0);
+                let imgNext = makeVar(pagsImgs, 0, 1);
+                let pagNext = makeVar(pagsImgs, 0, 0);
     
-    }, 0)
+                if (i !== pagsImgs.length-1) {
+                    imgNext = makeVar(pagsImgs, i+1, 1);
+                    pagNext = makeVar(pagsImgs, i+1, 0);
+                } 
     
-    setInterval(()=>{
-
-        setTimeout(()=>img1.classList.add(aIN),0)
-        setTimeout(()=>img2.classList.remove(aIN),0.06*time)
-        setTimeout(()=>img2.classList.add(aIN),time)
-        setTimeout(()=>img3.classList.remove(aIN),time+(0.06*time))
-        setTimeout(()=>img3.classList.add(aIN),time*2)        
-        setTimeout(()=>img1.classList.remove(aIN),2*time+(0.06*time))
-    
-    }, time*3)
+                    // animations add
+                    setTimeout(() => {
+                        img.classList.add(aIN);
+                        pag.classList.add(aPAG);
+                    }, time*i)
+                
+                    // animation remove
+                    setTimeout(() => {
+                        imgNext.classList.remove(aIN);
+                        pagNext.classList.remove(aPAG);
+                    }, (time*i+t))
+            }
+        }
+        
+        setTimeout(() => backend(), 0);
+        setInterval(() => backend(), time*pagsImgs.length);
 }
 
-// --> sets the "height" for slideshow to be responsive 
-// --> sets CSS variables when "slideshow" changes width, for animation to work properly
+// sets the "height" for slideshow to be responsive. sets CSS variables when "slideshow" changes width, for animation to work properly
 function responsiveWidth() {
 
     const slideshowWidth = slideshow.offsetWidth;
@@ -70,10 +107,14 @@ function responsiveWidth() {
 
   }
 
-//..................................................
 
 
-// d) DECLARATION
+// | CALL
+//----------------
 
-slideShow(imgShow1, imgShow2, imgShow3, 5000)
+// start value for slideshow container, if <= window width 
+responsiveWidth()
+
+// run slideshow
+slideShowAbstract(imgParArray, animations, 5000)
 
